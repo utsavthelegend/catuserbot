99c5aebb1a2116e896ddab2ec5885d9c281ca669
DARK_LIGHTNING_BOY <78705989+utsavthelegend@users.noreply.github.com>
2021-09-09 00:03:44 +0530

    Himimylove

--- userbot/assistant/hide.py
+++ userbot/assistant/hide.py
@@ -7,7 +7,7 @@ from telethon.events import CallbackQuery
 from userbot import catub
 
 
-@catub.tgbot.on(CallbackQuery(data=re.compile(b"hide_(.*)")))
+@himiub.tgbot.on(CallbackQuery(data=re.compile(b"hide_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
     if os.path.exists("./userbot/hide.txt"):
@@ -15,7 +15,7 @@ async def on_plug_in_callback_query_handler(event):
         try:
             reply_pop_up_alert = jsondata[f"{timestamp}"]["text"]
         except KeyError:
-            reply_pop_up_alert = "This message no longer exists in catub server"
+            reply_pop_up_alert = "This message no longer exists in himiub server"
     else:
         reply_pop_up_alert = "This message no longer exists "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
