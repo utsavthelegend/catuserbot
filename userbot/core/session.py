e39541a64f2e9841722e1269ebe4a4beb269dfeb
DARK_LIGHTNING_BOY <78705989+utsavthelegend@users.noreply.github.com>
2021-09-09 00:28:56 +0530

    Himiub

--- userbot/core/session.py
+++ userbot/core/session.py
@@ -4,7 +4,7 @@ from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 from telethon.sessions import StringSession
 
 from ..Config import Config
-from .client import CatUserBotClient
+from .client import himiUserBotClient
 
 __version__ = "2.10.6"
 
@@ -13,10 +13,10 @@ loop = None
 if Config.STRING_SESSION:
     session = StringSession(str(Config.STRING_SESSION))
 else:
-    session = "catuserbot"
+    session = "himiuserbot"
 
 try:
-    catub = CatUserBotClient(
+    Himiub = himiuserBotClient(
         session=session,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
@@ -31,8 +31,8 @@ except Exception as e:
     sys.exit()
 
 
-catub.tgbot = tgbot = CatUserBotClient(
-    session="CatTgbot",
+Himiub.tgbot = tgbot = HimiUserBotClient(
+    session="HimiTgbot",
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
     loop=loop,
