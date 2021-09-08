ff8699c8b044a4503610292adf9408d2a3978a07
DARK_LIGHTNING_BOY <78705989+utsavthelegend@users.noreply.github.com>
2021-09-09 00:26:29 +0530

    Himiub

--- userbot/core/decorators.py
+++ userbot/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "Only My Master can Access This !!\n\nDeploy your own Catuserbot."
+                or "Only My Owner can access this !!\n\nDeploy your own Himiusert."
             )
             await c_q.answer(
                 HELP_TEXT,
